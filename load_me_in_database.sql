557cc4b51517086b4db4b3a887040599371e96e3
zabbix-byte <zabbix@ztrunk.space>
2023-05-02 13:19:51 +0200

    adding database model

---
+++ load_me_in_database.sql
@@ -0,0 +1,14 @@
+/*
+INVENTORY ZTZBX SQL SCRIPT
+*/
+
+CREATE TABLE inventory (
+    username varchar(50) NOT NULL,
+    name varchar(50) NOT NULL,
+    quantity int NOT NULL DEFAULT 0, -- if the item is 0 is interpret like the item dont exits in the inventory
+    unit varchar(10) NOT NULL,
+    `image` varchar(100),
+    descriptiontitle varchar(100) NOT NULL,
+    `description` varchar(255) NOT NULL,
+    FOREIGN KEY (username) REFERENCES players(username)
+)ENGINE=InnoDB;
